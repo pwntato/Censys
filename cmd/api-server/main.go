77bf2ec49de823b8ac233f06dd2cbd45437133f6
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2025-09-23 17:42:28 -0700

    use appropriate contexts

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -71,7 +71,7 @@ func (s *APIServer) Set(c *gin.Context) {
 	}
 
 	// Call gRPC service
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(c, 5*time.Second)
 	defer cancel()
 
 	grpcResp, err := s.grpcClient.Set(ctx, &proto.SetRequest{
@@ -109,7 +109,7 @@ func (s *APIServer) Get(c *gin.Context) {
 	}
 
 	// Call gRPC service
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(c, 5*time.Second)
 	defer cancel()
 
 	grpcResp, err := s.grpcClient.Get(ctx, &proto.GetRequest{Key: key})
@@ -145,7 +145,7 @@ func (s *APIServer) Delete(c *gin.Context) {
 	}
 
 	// Call gRPC service
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(c, 5*time.Second)
 	defer cancel()
 
 	grpcResp, err := s.grpcClient.Delete(ctx, &proto.DeleteRequest{Key: key})
